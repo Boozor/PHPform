1e69f323ceed0a3bdd4c94c885bd281c747059c1
LeoVal1 <38884494+LeoVal1@users.noreply.github.com>
2020-08-09 07:00:09 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$connection = new MongoClient();
-$collection = $connection->database->collectionName;
-
-$collection->ensureIndex( array( "i" => 1 ) );  // create index on "i"
-$collection->ensureIndex( array( "i" => -1, "j" => 1 ) );  // index on "i" descending, "j" ascending
-?>
